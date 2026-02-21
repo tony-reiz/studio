161dd018cca33f250ca5fd959185bdc018c321e5
tony <bookline.businesspro@gmail.com>
2026-02-21 22:47:30 +0000

    C’est toujours long

--- src/components/bookline/bottom-nav.tsx
+++ src/components/bookline/bottom-nav.tsx
@@ -9,12 +9,12 @@ export function BottomNav() {
 
   return (
     <div className="fixed bottom-2 left-0 right-0 p-4 mb-4">
-      <div className="bg-secondary p-1 rounded-full flex items-center max-w-sm mx-auto shadow-lg">
+      <div className="bg-secondary p-1 rounded-full flex items-center max-w-[16rem] mx-auto shadow-lg">
         <Link href="/home" legacyBehavior passHref>
           <a
             className={cn(
               "w-full text-center rounded-full py-3 text-base font-semibold transition-all duration-300 ease-in-out",
-              pathname === '/home'
+              pathname === '/home' || pathname === '/'
                 ? "bg-foreground text-background"
                 : "text-foreground bg-transparent"
             )}
