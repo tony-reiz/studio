f11b22c9d0f1f7e4b9f5e98e11061265b230b12c
tony <bookline.businesspro@gmail.com>
2026-02-21 20:32:44 +0000

    Non fin je veux une nouvelle page

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -1,11 +1,14 @@
+import { SellHeader } from '@/components/bookline/sell-header';
+import { SellForm } from '@/components/bookline/sell-form';
 import { BottomNav } from '@/components/bookline/bottom-nav';
 
 export default function SellPage() {
   return (
     <div className="flex flex-col items-center min-h-screen bg-background text-foreground">
       <div className="w-full max-w-md mx-auto flex flex-col flex-1">
+        <SellHeader />
         <main className="flex flex-col items-center w-full flex-1 px-4 sm:px-6 pb-28">
-          {/* Page vierge */}
+          <SellForm />
         </main>
       </div>
       <BottomNav />
