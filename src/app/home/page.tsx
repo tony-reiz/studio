a5cf2e50caf43d2c165c8406357110fcac5d0901
tony <bookline.businesspro@gmail.com>
2026-02-21 22:51:04 +0000

    Non je veux vraiment tout a gauche, aussi le « bonjour alex »  et « book

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
     <div className="flex flex-col items-center min-h-screen bg-background text-foreground">
       <div className="w-full max-w-md mx-auto flex flex-col flex-1">
         <Header />
-        <main className="flex flex-col items-center w-full flex-1 pb-28">
+        <main className="flex flex-col w-full flex-1 pb-28">
           <SearchSection />
           <EbookDisplayArea />
         </main>
