ceb394269ad9585267dc892c06c44df0ff3673a6
tony <bookline.businesspro@gmail.com>
2026-02-23 22:32:40 +0000

    Pourtant can’a pas fonctionné, regarde bien de ton coté si y a une erreu

--- src/components/bookline/sell-form.tsx
+++ src/components/bookline/sell-form.tsx
@@ -115,10 +115,10 @@ export function SellForm() {
                 <p>votre gain net</p>
                 <p>total de l'ebook</p>
             </div>
-            <div className='flex-shrink-0 bg-foreground text-background rounded-l-[30px] px-12 py-4 space-y-1 text-sm flex flex-col justify-center'>
-                <p className="text-right w-full">{formatPrice(ebookPrice)}</p>
-                <p className="text-right w-full">{formatPrice(netGain)}</p>
-                <p className="text-right w-full">{formatPrice(totalPriceForCustomer)}</p>
+            <div className='flex-shrink-0 bg-foreground text-background rounded-l-[30px] px-12 py-4 space-y-1 text-sm flex flex-col justify-center items-end'>
+                <p>{formatPrice(ebookPrice)}</p>
+                <p>{formatPrice(netGain)}</p>
+                <p>{formatPrice(totalPriceForCustomer)}</p>
             </div>
         </div>
     </div>
