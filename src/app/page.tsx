b572b794eea3f75555cbd1daa1d4893d03afde90
tony <bookline.businesspro@gmail.com>
2026-02-25 19:17:15 +0000

    Bon regarde, tu vois bien que c est trop bas le bouton? Alors remonte be

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function LandingPage() {
           <GraduationCap className="h-5 w-5 opacity-50" />
         </div>
       </header>
-      <main className="flex-1 flex flex-col items-center justify-center text-center relative px-4 pb-40 sm:pb-24">
+      <main className="flex-1 flex flex-col items-center justify-center text-center relative px-4 pb-48 sm:pb-36">
         {/* Background cards */}
         <div className="absolute inset-0 w-full h-full overflow-hidden -z-10 flex items-center justify-center">
             <div className="relative w-full max-w-lg h-96">
@@ -34,7 +34,7 @@ export default function LandingPage() {
           </p>
         </div>
       </main>
-      <footer className="absolute bottom-24 sm:bottom-12 left-0 right-0 px-6 flex justify-center">
+      <footer className="absolute bottom-32 sm:bottom-24 left-0 right-0 px-6 flex justify-center">
         <Link href="/home" passHref>
           <Button className="bg-foreground text-background rounded-full h-14 px-12 text-lg font-semibold hover:bg-foreground/90">
             commencer
