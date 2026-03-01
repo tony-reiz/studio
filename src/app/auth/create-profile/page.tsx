ef42c3bca74055ae69a59608ebea5060ee17fb99
tony <bookline.businesspro@gmail.com>
2026-03-01 11:09:20 +0000

    Tu peux vraiment baisser la taille mas vraiment juuuuste un tout petit p

--- src/app/auth/create-profile/page.tsx
+++ src/app/auth/create-profile/page.tsx
@@ -103,7 +103,7 @@ export default function CreateProfilePage() {
             <div className="flex flex-col items-start">
               <div className="-mt-1">
                 <p className="text-[24px] font-bold tracking-widest text-foreground">FINALISEZ</p>
-                <h1 className="text-5xl sm:text-6xl font-extrabold text-primary -mt-1">L’INSCRIPTION !</h1>
+                <h1 className="text-[46px] sm:text-[58px] font-extrabold text-primary -mt-1">L’INSCRIPTION !</h1>
               </div>
             </div>
         </header>
