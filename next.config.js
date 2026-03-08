5d354121be45a0b5f1767be0f047e6c1e4f5a7c9
tony <bookline.businesspro@gmail.com>
2026-03-08 22:25:48 +0000

    bah non toujours pas, corrige ce bug pour que je puisse tester la PWA st

---
+++ next.config.js
@@ -0,0 +1,39 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('@ducanh2912/next-pwa')({
+  dest: 'public',
+  disable: false,
+});
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
