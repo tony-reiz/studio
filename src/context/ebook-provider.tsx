7cdd8cf5fa953a04481a3886b51bb5d0fc2b6c14
tony <bookline.businesspro@gmail.com>
2026-03-09 21:36:41 +0000

    t'es au courant que c'est le pdf de la page "vendre" qui doit s'afficher

--- src/context/ebook-provider.tsx
+++ src/context/ebook-provider.tsx
@@ -6,9 +6,7 @@ import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { translations, type Locale } from '@/lib/translations';
 import { pdfjs } from 'react-pdf';
 
-if (typeof window !== 'undefined') {
-  pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.js`;
-}
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 // Define the shape of an ebook
 export interface Ebook {
