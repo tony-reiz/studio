8ac0ce04b4550b974ac6e84662462e36aaff491c
tony <bookline.businesspro@gmail.com>
2026-03-15 14:06:45 +0000

    en fait c'est dans la version mobile maintenant je veux que tu monte le

--- src/app/(bookline)/sell/page.tsx
+++ src/app/(bookline)/sell/page.tsx
@@ -204,7 +204,7 @@ export default function SellPage() {
                 <SellForm />
               </div>
             </div>
-            <div className="max-w-[16rem] w-full">
+            <div className="max-w-[16rem] w-full -mt-4 md:mt-0">
                 <Button
                     type="submit"
                     disabled={isButtonDisabled}
